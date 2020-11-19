720ef671deea5dba94eca219aa90ad787406a4e9
yangzl <youngzil@163.com>
2020-11-19 14:50:34 +0800

    feat(Shell脚本): shell打包脚本
    Signed-off-by: yangzl <youngzil@163.com>

--- quickstart-linux/docs/linux/asiainfo/aif/gateway/git-aifgw-deploy.sh
+++ quickstart-linux/docs/linux/asiainfo/aif/gateway/git-aifgw-deploy.sh
@@ -63,7 +63,7 @@ mkdir -p $CODE_PATH
 mkdir -p $TAR_PACKAGE_PATH
 
 gitClone(){
-  echo "[echo] 开始下载源码..."
+  echo "[INFO] 开始下载源码..."
   cd $CODE_PATH
   rm -rf * .[!.]*
   #git clone http://yangzl:nihao%40124@10.19.14.241/OSP/gateway.git $CODE_PATH
@@ -77,7 +77,7 @@ gitClone(){
 }
 
 checkVueEnvironment(){
-  echo "[echo] 开始检查Vue环境..."
+  echo "[INFO] 开始检查Vue环境..."
   export NVM_DIR="$HOME/.nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
@@ -90,10 +90,10 @@ checkVueEnvironment(){
 
 #  npm i -g aid-cli@2.0.0 --sass-binary-site=http://npm.taobao.org/mirrors/node-sass/
 
-  echo "[echo] NVM版本:" `nvm --version`
-  echo "[echo] NodeJS版本:" `node -v`
-  echo "[echo] npm版本:" `npm -v`
-  echo "[echo] aid版本:" `aid -v`
+  echo "[INFO] NVM版本:" `nvm --version`
+  echo "[INFO] NodeJS版本:" `node -v`
+  echo "[INFO] npm版本:" `npm -v`
+  echo "[INFO] aid版本:" `aid -v`
 }
 
 setProjectVersion(){
@@ -107,10 +107,10 @@ setProjectVersion(){
   PROJECT_VERSION=$($MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout)
   if [[ $PROJECT_VERSION == *$ERROR_FLAG* ]]
   then
-      echo "项目${PROJECT_NAME}的POM配置文件有误，请检查"
-      echo $PROJECT_VERSION
+      echo "[ERROR] 项目${PROJECT_NAME}的POM配置文件有误，请检查"
+      echo "[ERROR] $PROJECT_VERSION"
   else
-      echo "当前正在打包[分支=${BRANCH_NAME},工程=${PROJECT_NAME},版本=${PROJECT_VERSION}]"
+      echo "[INFO] 当前正在打包[分支=${BRANCH_NAME},工程=${PROJECT_NAME},版本=${PROJECT_VERSION}]"
   fi
 }
 
@@ -123,17 +123,17 @@ aifgw()
   TARGET_NAME=aifgw-backend-${PROJECT_VERSION}.tar.gz
 
   cd $CODE_PATH/$PROJECT_NAME
-  echo "[echo] 开始编译${PROJECT_NAME}..."
+  echo "[INFO] 开始编译${PROJECT_NAME}..."
 
   if [ -d $CODE_PATH/local/$PROJECT_NAME ]; then
-    echo "[echo] 开始替换代码${PROJECT_NAME}..."
+    echo "[INFO] 开始替换代码${PROJECT_NAME}..."
     cp -rf $CODE_PATH/local/$PROJECT_NAME $CODE_PATH/$PROJECT_NAME
   fi
 
   $MAVEN_HOME/mvn -Prelease-all -Pbuild-$PROFILE_ENV -DskipTests clean install -U
 
   if [ ! -f $CODE_PATH/$PROJECT_NAME/aifgw-distribution/target/$TARGET_NAME ];then
-          echo "打包失败,请确认${PROJECT_NAME}打包是否成功"
+          echo "[ERROR] 打包失败,请确认${PROJECT_NAME}打包是否成功"
           exit 1
   fi
   rm -rf $TAR_PACKAGE_PATH/$TARGET_NAME
@@ -141,7 +141,7 @@ aifgw()
   # cd $TAR_PACKAGE_PATH/;mkdir temp;cd temp;tar zxf ../aifgw-backend-1.0.tar.gz;cd aifgw-backend-1.0/conf;jar xf ../lib/aifgw-backend-boot-1.0.jar
   # cd $TAR_PACKAGE_PATH/temp;tar czf aifgw-backend-1.0.tar.gz aifgw-backend-1.0;mv aifgw-backend-1.0.tar.gz ~/deploy_oppf/dist_gateway/
   # cd $TAR_PACKAGE_PATH;rm -rf $TAR_PACKAGE_PATH/temp
-  echo "[echo] 项目${PROJECT_NAME}编译完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
+  echo "[INFO] 项目${PROJECT_NAME}编译完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
 }
 
 oauth()
@@ -153,22 +153,22 @@ oauth()
   TARGET_NAME=aifgw-security-${PROJECT_VERSION}.tar.gz
 
   cd $CODE_PATH/$PROJECT_NAME
-  echo "[echo] 开始编译$PROJECT_NAME..."
+  echo "[INFO] 开始编译$PROJECT_NAME..."
 
   if [ -d $CODE_PATH/local/$PROJECT_NAME ]; then
-    echo "[echo] 开始替换代码${PROJECT_NAME}..."
+    echo "[INFO] 开始替换代码${PROJECT_NAME}..."
     cp -rf $CODE_PATH/local/$PROJECT_NAME $CODE_PATH/$PROJECT_NAME
   fi
 
   $MAVEN_HOME/mvn -Pbuild-$PROFILE_ENV -DskipTests clean install -U
 
   if [ ! -f $CODE_PATH/$PROJECT_NAME/aifgw-security-distribution/target/$TARGET_NAME ];then
-          echo "打包失败,请确认${PROJECT_NAME}打包是否成功"
+          echo "[ERROR] 打包失败,请确认${PROJECT_NAME}打包是否成功"
           exit 1
   fi
   rm -rf $TAR_PACKAGE_PATH/$TARGET_NAME
   mv  $CODE_PATH/$PROJECT_NAME/aifgw-security-distribution/target/$TARGET_NAME   $TAR_PACKAGE_PATH/$TARGET_NAME
-  echo "[echo] 项目${PROJECT_NAME}编译完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
+  echo "[INFO] 项目${PROJECT_NAME}编译完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
 }
 
 webapp()
@@ -177,10 +177,10 @@ webapp()
   TARGET_NAME=gateway-console-server.tar.gz
 
   cd $CODE_PATH/$PROJECT_NAME
-  echo "[echo] 开始编译${PROJECT_NAME}..."
+  echo "[INFO] 开始编译${PROJECT_NAME}..."
 
   if [ -d $CODE_PATH/local/$PROJECT_NAME ]; then
-    echo "[echo] 开始替换代码${PROJECT_NAME}..."
+    echo "[INFO] 开始替换代码${PROJECT_NAME}..."
     cp -rf $CODE_PATH/local/$PROJECT_NAME $CODE_PATH/$PROJECT_NAME
   fi
 
@@ -194,12 +194,12 @@ webapp()
   $MAVEN_HOME/mvn -Prelease-all -DskipTests clean install -U
 
   if [ ! -f $CODE_PATH/$PROJECT_NAME/aifgw-web-app-distribution/target/$TARGET_NAME ];then
-          echo "打包失败,请确认${PROJECT_NAME}打包是否成功"
+          echo "[ERROR] 打包失败,请确认${PROJECT_NAME}打包是否成功"
           exit 1
   fi
   rm -rf $TAR_PACKAGE_PATH/$TARGET_NAME
   mv  $CODE_PATH/$PROJECT_NAME/aifgw-web-app-distribution/target/$TARGET_NAME  $TAR_PACKAGE_PATH/$TARGET_NAME
-  echo "[echo] 项目${PROJECT_NAME}编译完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
+  echo "[INFO] 项目${PROJECT_NAME}编译完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
 }
 
 webdev()
@@ -209,17 +209,17 @@ webdev()
   TARGET_NAME=webdev.war
 
   cd $CODE_PATH/$PROJECT_NAME
-  echo "[echo] 开始编译${PROJECT_NAME}..."
+  echo "[INFO] 开始编译${PROJECT_NAME}..."
 
   cp ${HOME}/deploy_oppf/git_gateway/node_modules.tar.gz .
   tar -xzvf node_modules.tar.gz  > /dev/null
 
 #  npm i -g aid-cli@2.0.0 --sass-binary-site=http://npm.taobao.org/mirrors/node-sass/
-#  echo "[echo] aid版本:" `aid -v`
+#  echo "[INFO] aid版本:" `aid -v`
 
   #code 覆盖
   if [ -d $CODE_PATH/local/$PROJECT_NAME ]; then
-    echo "[echo] 开始替换代码${PROJECT_NAME}..."
+    echo "[INFO] 开始替换代码${PROJECT_NAME}..."
     cp -rf $CODE_PATH/local/$PROJECT_NAME $CODE_PATH/$PROJECT_NAME
   fi
   #修改配置文件：替换routes.js文件
@@ -227,13 +227,13 @@ webdev()
 
   aid build -N
   if [ -d "dist" ];then
-  echo "开始打包${PROJECT_NAME}的war包..."
+  echo "[INFO] 开始打包${PROJECT_NAME}的war包..."
       cd dist
       jar cf $TARGET_NAME *
       rm -rf $TAR_PACKAGE_PATH/$TARGET_NAME
       mv $TARGET_NAME $TAR_PACKAGE_PATH/
   fi
-  echo "${TARGET_NAME}打包完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
+  echo "[INFO] ${TARGET_NAME}打包完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
 }
 
 webopr()
@@ -243,17 +243,17 @@ webopr()
   TARGET_NAME=webopr.war
 
   cd $CODE_PATH/$PROJECT_NAME
-  echo "[echo] 开始编译${PROJECT_NAME}..."
+  echo "[INFO] 开始编译${PROJECT_NAME}..."
 
   cp ${HOME}/deploy_oppf/git_gateway/node_modules.tar.gz .
   tar -xzvf node_modules.tar.gz  > /dev/null
 
 #  npm i -g aid-cli@2.0.0 --sass-binary-site=http://npm.taobao.org/mirrors/node-sass/
-#  echo "[echo] aid版本:" `aid -v`
+#  echo "[INFO] aid版本:" `aid -v`
 
   #code 覆盖
   if [ -d $CODE_PATH/local/$PROJECT_NAME ]; then
-    echo "[echo] 开始替换代码${PROJECT_NAME}..."
+    echo "[INFO] 开始替换代码${PROJECT_NAME}..."
     cp -rf $CODE_PATH/local/$PROJECT_NAME $CODE_PATH/$PROJECT_NAME
   fi
   #替换routes.js文件
@@ -261,13 +261,13 @@ webopr()
 
   aid build -N
   if [ -d "dist" ];then
-  echo "开始打包$TARGET_NAME的war包..."
+  echo "[INFO] 开始打包$TARGET_NAME的war包..."
       cd dist
       jar cf $TARGET_NAME *
       rm -rf $TAR_PACKAGE_PATH/$TARGET_NAME
       mv $TARGET_NAME $TAR_PACKAGE_PATH/
   fi
-  echo "${TARGET_NAME}打包完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
+  echo "[INFO] ${TARGET_NAME}打包完成,打包文件的文件请查看$TAR_PACKAGE_PATH/$TARGET_NAME..."
 }
 
 
@@ -316,5 +316,5 @@ esac
 END_DATE=$(date +%s)
 STAMP_DIFF=`expr $END_DATE - $START_DATE`
 
-echo "#####aifgw 版本:${ospversion}#####"
-echo "打包完成,耗时=${STAMP_DIFF}秒，打包后的压缩包请查看$TAR_PACKAGE_PATH..."
+echo "[INFO] #####aifgw 版本:${ospversion}#####"
+echo "[INFO] 打包完成,耗时=${STAMP_DIFF}秒，打包后的压缩包请查看$TAR_PACKAGE_PATH..."
